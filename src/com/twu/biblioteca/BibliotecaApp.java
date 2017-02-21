24b263972789dc760fdcf30403dce250f2f3fa11
Unicornelia <ez.kornelia@gmail.com>
2017-02-21 16:45:35 +0000

    Starting biblioteca

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+
         System.out.println("Hello visitor, the Biblioteca is open!");
     }
 }
