3bb7239b8a7c81dfc155ec44730b04fba99aaa10
Kornelia Szabo <korneliaszabo@Kornelias-MacBook-Pro.local>
2017-03-03 18:05:10 +0000

    Adding menu class and input taking from user

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,11 +5,13 @@ public class BibliotecaApp {
     public static void main(String[] args) {
 
         Biblioteca biblioteca = new Biblioteca();
+        Menu menu = new Menu();
+
         System.out.println(biblioteca.printWelcome());
         System.out.println("");
 
-        biblioteca.listBooks();
-        biblioteca.listBooksFormatted();
+        int option = menu.intro();
+        menu.menuOptions(option, biblioteca);
     }
 }
 
